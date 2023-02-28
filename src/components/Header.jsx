15cb65a94ca5ab1371c9356c74777548a11de224
vishnubansal001 <vishnu1549.be21@chitkara.edu.in>
2023-03-01 01:42:29 +0530

    Formatted the whole project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,42 @@
-import React, { useState } from 'react';
-import {AiOutlineMenu,AiOutlineClose} from 'react-icons/ai';
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
-    const [toggle,setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false);
   return (
-    <div className='bg-[#2699fb] p-4'>
-      <div className='max-w-[1240px] py-[10px] flex justify-between items-center mx-auto'>
-        <div className='text-3xl font-bold'>WsCube Tech</div>
-        { 
-        !toggle 
-        ? 
-        <AiOutlineMenu onClick={() => setToggle(!toggle)} className='text-white text-2xl md:hidden block'/> 
-        :
-        <AiOutlineClose onClick={() => setToggle(!toggle)} className='text-white text-2xl md:hidden block'/>
-        }
-        <ul className='hidden md:flex text-white gap-10'>
-            <li>Home</li>
-            <li>Company</li>
-            <li>Resources</li>
-            <li>About</li>
-            <li>Contact</li>
+    <div className="bg-[#2699fb] p-4">
+      <div className="max-w-[1240px] py-[10px] flex justify-between items-center mx-auto">
+        <div className="text-3xl font-bold">WsCube Tech</div>
+        {!toggle ? (
+          <AiOutlineMenu
+            onClick={() => setToggle(!toggle)}
+            className="text-white text-2xl md:hidden block"
+          />
+        ) : (
+          <AiOutlineClose
+            onClick={() => setToggle(!toggle)}
+            className="text-white text-2xl md:hidden block"
+          />
+        )}
+        <ul className="hidden md:flex text-white gap-10">
+          <li>Home</li>
+          <li>Company</li>
+          <li>Resources</li>
+          <li>About</li>
+          <li>Contact</li>
         </ul>
-        <ul className={`duration-300 md:hidden w-full h-screen fixed bg-black top-[88px] text-white gap-10 ${toggle ? 'left-0' : 'left-[-100%]'}`}>
-            <li className='p-5'>Home</li>
-            <li className='p-5'>Company</li>
-            <li className='p-5'>Resources</li>
-            <li className='p-5'>About</li>
-            <li className='p-5'>Contact</li>
+        <ul
+          className={`duration-300 md:hidden w-full h-screen fixed bg-black top-[88px] text-white gap-10 ${
+            toggle ? "left-0" : "left-[-100%]"
+          }`}
+        >
+          <li className="p-5">Home</li>
+          <li className="p-5">Company</li>
+          <li className="p-5">Resources</li>
+          <li className="p-5">About</li>
+          <li className="p-5">Contact</li>
         </ul>
       </div>
     </div>
-  )
+  );
 }
